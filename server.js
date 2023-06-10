94bf3c36ba1b9f6b5c9290b4f68dddce16d0ed78
Harpreet Singh <harpreet95hs@gmail.com>
2023-06-10 15:01:58 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express()
+
+app.listen(3000,() => {
+    console.log("Listening")
+})
+
+app.get("/greeting/:name", (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello, ${name}!`)
+})
