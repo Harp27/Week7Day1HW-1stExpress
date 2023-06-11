cf0c4c880e626f46c3c6497cdf23a3cb399f813a
Harpreet Singh <harpreet95hs@gmail.com>
2023-06-11 16:28:42 -0400

    completed

--- pass-it-around/server2.js
+++ pass-it-around/server2.js
@@ -7,5 +7,21 @@ app.listen(3001,() => {
 })
 
 app.get("/", (req, res) => {
-    res.send(`99 Bottles of beer on the wall`)
-})
+    res.send(`
+    <h1>99 Bottles of beer on the wall</h1>
+    <a href="/98">Take one down, pass it around</a>`)
+});
+
+app.get("/:number_of_bottles", (req, res) => {
+    const numberOfBottles = parseInt(req.params.number_of_bottles);
+    const nextNumber = numberOfBottles - 1;
+
+const link = numberOfBottles > 0 ? `<a href="/${nextNumber}">Take one down pass it around</a>` : "";
+const startOverLink = `<a href="/">Start over</a>`
+
+res.send(`
+    <h1>${numberOfBottles} Bottles of beer of the wall</h1>
+    ${link}
+    ${startOverLink}
+    `)
+});
